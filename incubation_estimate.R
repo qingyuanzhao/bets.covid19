1cfd142b2587d5c0568be91e84f717502639f720
Phyllis Ju <phyllis.nju@gmail.com>
2020-02-10 17:24:00 +0000

    changed subset criterion

--- incubation_estimate.R
+++ incubation_estimate.R
@@ -15,11 +15,18 @@ case72$Infected_first <- date.process("15-Jan")
 case72$Infected_last <- date.process("21-Jan")
 data <- rbind(data, case72)
     
-    
-## Only consider cases with known symptom onset, arrived on or before January 23 
+## Only consider cases with known symptom onset
 data <- subset(data, !is.na(Symptom))
-data <- subset(data, Arrived <= 23+31)
-data <- subset(data, !(is.na(Arrived) & Infected_first == 1 & Infected_last == Symptom)) # remove cases with no information
+## only consider cases arrived on or before January 23 
+# data <- subset(data, Arrived <= 23+31) ## this will remove outside cases
+## remove cases with no infection informations
+# data <- subset(data, !(is.na(Arrived) & Infected_first == 1 & Infected_last == Symptom)) # remove cases with no information
+# data <- subset(data, !(is.na(Arrived))) ## only consider outside cases
+data <- subset(data, Infected_first != 1)
+data <- subset(data, Infected_last != Symptom)
+dim(data)
+hist(data$Symptom - data$Infected_last)
+hist(data$Symptom - data$Infected_first)
 
 #' Compute the likelihood
 #'
@@ -36,14 +43,14 @@ infection.likelihood <- function(symptom, infected_first, infected_last, GT) {
 }
 
 myfun <- function(par) { 
-    GT <- R0::generation.time("gamma", par,truncate = 100); 
+    GT <- R0::generation.time("lognormal", par,truncate = 100); 
     infection.likelihood(data$Symptom, data$Infected_first, data$Infected_last, GT) 
 }
 
 fit <- optim(c(7.5, 3.4), myfun, control = list(fnscale = -1))
 print(fit)
 
-pars <- expand.grid(mean = seq(5, 15, 0.1), sd = seq(2, 10, 0.1))
+pars <- expand.grid(mean = seq(3, 15, 0.05), sd = seq(2, 8, 0.05))
 pars$loglike <- apply(pars, 1, myfun)
 print(pars[which.max(pars$loglike),]) ## print the grid-search MLE to terminal
 pars$in.CR <- (pars$loglike > fit$value - qchisq(0.95, 1) / 2)
@@ -54,3 +61,4 @@ p1
 
 p2 <- ggplot(pars) + aes(x = mean, y = sd, fill = in.CR) +geom_tile()
 p2
+
